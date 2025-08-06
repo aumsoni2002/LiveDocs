31e4ba5de93d1331caa1dc78758189ab56675b8e
Aum Soni <96745193+aumsoni2002@users.noreply.github.com>
2025-08-05 23:32:55 -0400

    feat: Integrate Sentry to identify and debug errors and performance

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://5a182d2087f372d57b7d4c3480b78fb8@o4509794704097280.ingest.us.sentry.io/4509794718253056",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});

124bbfefed707ebbe1894512a7eb37a6ecc22966
Aum Soni <96745193+aumsoni2002@users.noreply.github.com>
2025-08-07 13:12:32 -0400

    feat: Users can invite other users to edit the document

--- components/ShareModal.tsx
+++ components/ShareModal.tsx
@@ -13,6 +13,8 @@ import { Button } from "./ui/button";
 import Image from "next/image";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
+import UserTypeSelector from "./UserTypeSelector";
+import Collaborator from "./Collaborator";
 
 const ShareModal = ({
   roomId,
@@ -66,7 +68,30 @@ const ShareModal = ({
               onChange={(e) => setEmail(e.target.value)}
               className="share-input"
             />
+            <UserTypeSelector userType={userType} setUserType={setUserType} />
           </div>
+          <Button
+            type="submit"
+            onClick={shareDocumentHandler}
+            className="gradient-blue flex h-full gap-1 px-5"
+            disabled={loading}
+          >
+            {loading ? "Sending..." : "Invite"}
+          </Button>
+        </div>
+        <div className="my-2 space-y-2">
+          <ul className="flex flex-col">
+            {collaborators.map((collaborator) => (
+              <Collaborator
+                key={collaborator.id}
+                roomId={roomId}
+                creatorId={creatorId}
+                email={collaborator.email}
+                collaborator={collaborator}
+                user={user.info}
+              />
+            ))}
+          </ul>
         </div>
       </DialogContent>
     </Dialog>

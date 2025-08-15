e9639a9314706426bfee3251447ef8948a360523
Aum Soni <96745193+aumsoni2002@users.noreply.github.com>
2025-08-15 17:19:04 -0400

    temporary bug fix: Users can invite other non existing users

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -18,12 +18,17 @@ const Document = async ({ params: { id } }: SearchParamProps) => {
   const userIds = Object.keys(room.usersAccesses);
   const users = await getClerkUsers({ userIds });
 
-  const usersData = users.map((user: User) => ({
-    ...user,
-    userType: room.usersAccesses[user.email]?.includes("room:write")
-      ? "editor"
-      : "viewer",
-  }));
+  const usersData = users.map((user: User) => {
+    if (!user || !user.email) {
+      return { ...user, userType: "viewer" };
+    }
+    return {
+      ...user,
+      userType: room.usersAccesses[user.email]?.includes("room:write")
+        ? "editor"
+        : "viewer",
+    };
+  });
 
   const currentUserType = room.usersAccesses[
     clerkUser.emailAddresses[0].emailAddress
